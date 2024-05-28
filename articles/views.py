dc631962e656978ae2a31cbcdc57d44f15f2eca3
Josh Hawes <joshua.m.hawes@gmail.com>
2024-05-28 07:47:12 +0000

    created article search function

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Article
 
-# Create your views here.
 
 def all_articles(request):
 
     articles = Article.objects.all()
+
+    query = None
+
+    if request.GET:
+        if "q" in request.GET:
+            query = request.GET["q"]
+            if not query:
+                messages.error(request, "you've entered no search crieteria")
+                return redirect(reverse("articles"))
+
+            queries = Q(title__icontains=query) | Q(keywords__icontains=query)
+            articles = articles.filter(queries)
+
     context = {
-        'articles': articles,
+        "articles": articles,
+        "search_term": query,
     }
-    return render(request, 'articles/articles.html', context)
+
+    return render(request, "articles/articles.html", context)
